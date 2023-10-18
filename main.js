6fa5d0b19f18b053eca5c973ca7b62d0e4aefac4
Oscar Barreto <ojbarreto9@hotmail.com>
2023-10-18 16:16:47 -0500

    PreEntrega3Barreto

--- main.js
+++ main.js
@@ -1,74 +1,72 @@
 
-alert ("Usted se ha ganado un codigo promocional: OSCAR10")
+alert("¡Usted se ha ganado un código promocional: OSCAR10!");
 
-const carroDeCompras = []
+const carroDeCompras = JSON.parse(localStorage.getItem('carrito')) || [];
 
-function Producto (nombre, precio, marca){
-    this.nombre=nombre;
-    this.precio=precio;
-    this.marca=marca;
+function Producto(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
 }
 
-let producto1 = new Producto ("proteina", 250000, "carnivor")
-let producto2 = new Producto ("creatina", 150000, "universal")
-let producto3 = new Producto ("aminoacidos", 180000, "metrx")
-let producto4 = new Producto ("pre-entreno", 150000, "c4")
-let producto5 = new Producto ("multivitaminico", 220000, "animal")
-
-let lista = [producto1, producto2, producto3, producto4, producto5]
-
-
-function precioDelCarrito (){
-    return carroDeCompras.reduce((acumulado,x) => (x.precio+acumulado), 0)
+function agregarAlCarrito(nombre, precio) {
+    const producto = new Producto(nombre, precio);
+    carroDeCompras.push(producto);
+    localStorage.setItem('carrito', JSON.stringify(carroDeCompras));
+    mostrarCarrito();
 }
 
-function agregarAlCarrito (){
-    let pregunta = prompt (`Quieres agregar un producto?`).toLocaleLowerCase()
+function precioDelCarrito() {
+    return carroDeCompras.reduce((acumulado, producto) => producto.precio + acumulado, 0);
+}
 
-    if (pregunta == "si"){
-        
-        let nuevoProducto = prompt(`Que producto quieres agregar?
-        1. Proteina
-        2. Creatina 
-        3. Aminoacidos
-        4. Pre-Entreno
-        5. Multivitaminico`).toLowerCase().trim();
+function mostrarCarrito() {
+    const listaCarrito = document.getElementById('lista-carrito');
+    const totalCarrito = document.getElementById('total');
+
+    // Limpiar la lista antes de volver a dibujarla
     
-        let existe = lista.reduce((acomulado, x) => (x.nombre == nuevoProducto) || acomulado, false)
-    
-        if (existe){
-            let agregar = lista.filter (x => x.nombre == nuevoProducto)
-            carroDeCompras.push(agregar[0])
-            agregarAlCarrito ();
+    listaCarrito.innerHTML = ''; 
 
-        } else {
-            console.log(existe)
-    
-        alert ("Este producto no se encontro")    
-        agregarAlCarrito()
-        }
-    }
-    else if (pregunta=="no"){
-        let precioFinal = precioDelCarrito();
-        alert(precioFinal==0?"Tu carro de compras esta vacio":"Este es el precio de tus productos : $"+precioFinal+" sin descuento y sin IVA.")
-    }
-    else {
-        alert(`Respuesta Incorrecta`)
-        agregarAlCarrito();
-    }
+    let total = 0;
 
+    carroDeCompras.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.nombre} - $${item.precio}`;
+        listaCarrito.appendChild(li);
+
+        total += item.precio;
+    });
+
+    totalCarrito.textContent = total;
 }
 
+function aplicarDescuento() {
+    const cuponInput = document.getElementById('cupon');
+    const codigoCupon = cuponInput.value.trim().toUpperCase();
 
-agregarAlCarrito()
-
-
-let codigo = prompt ("Ingrese su codigo promocional (si NO tiene, escriba NO)")
-
-if (codigo=="OSCAR10"){
-    alert ("El precio de sus productos es de $"+ (precioDelCarrito ()*0.9*1.16 ))
+    if (codigoCupon === "OSCAR10") {
+        const precioConDescuento = precioDelCarrito() * 0.9 * 1.16;
+        alert(`El precio de sus productos con descuento es de $${precioConDescuento.toFixed(2)}. ¡Gracias por utilizar el código OSCAR10!`);
+    } else {
+        alert("Código promocional no válido. El precio de sus productos es de $" + (precioDelCarrito() * 1.16).toFixed(2));
+    }
 }
-else{
-    alert  ("El precio de sus productos es de $"+ (precioDelCarrito()*1.16))
+
+function realizarCompra() {
+    const precioFinal = precioDelCarrito();
+    alert(`El precio de sus productos es de $${(precioFinal * 1.16).toFixed(2)} sin descuento y con IVA.`);
 }
 
+function limpiarCarrito() {
+    carroDeCompras.length = 0;
+    localStorage.setItem('carrito', JSON.stringify(carroDeCompras));
+    mostrarCarrito();
+}
+
+function ejecutarCarrito() {
+    document.getElementById("productos").style.display = "block";
+    document.getElementById("carrito").style.display = "block";
+    mostrarCarrito();
+}
+
+ejecutarCarrito();
